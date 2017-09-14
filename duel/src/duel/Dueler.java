036f7e6ca1318de1fcbdcb0d0de5c25b6f9df51c
steph_000 <steph_000@stephanie>
2017-09-13 21:36:05 -0400

    duel

---
+++ duel/src/duel/Dueler.java
@@ -0,0 +1,17 @@
+package duel;
+
+public class Dueler {
+
+	//print a taunt
+	//later: the taunt must be different each time the method is called
+	void taunt() {
+	
+	String output = taunt; 
+	System.out.println(output);
+	
+	}
+	
+	//returns the name of the Dueler
+	String getName();
+		
+}
