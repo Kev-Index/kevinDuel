a8ba83e7cbd9a342281fa22e35477eef7f01f9b6
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-09-27 15:31:21 -0400

    dd

--- duel/src/duel/CharacterB.java
+++ duel/src/duel/CharacterB.java
@@ -4,49 +4,51 @@ public class CharacterB implements Dueler
 {
 	private int hp; 
 	private boolean isLoaded = false;
-	
+
 	public CharacterB() {
 	}
-		public void taunt() {
-			String taunt = ("yo mama so fat");
-			System.out.println(taunt);
+	public void taunt() {
+		String taunt = ("yo mama so fat");
+		System.out.println(taunt);
+	}
+	public String getName() {
+		String name = ("Carlos");
+		return name;
+	}
+	public void setStartingHP(int hp) {
+		this.hp = hp;
+	}
+	public int getHP() {
+		return hp;
+	}
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		if (d.getHP() == hp) {
+			return true;
 		}
-		public String getName() {
-			String name = ("Carlos");
-			return name;
-		}
-		public void setStartingHP(int hp) {
-			this.hp = hp;
-		}
-		public int getHP() {
-			return hp;
-		}
-		public boolean determineIfOpponentIsFair(Dueler d, int hp) {
-			if (d.getHP() == hp) {
-				return true;
-			}
-			return false;
-		}
-		public int getAction(Object caller) {
-			if(caller instanceof Duel) {
-				if(Math.random() > 0.5) {
-					if (isLoaded == false) {
+		return false;
+	}
+	public int getAction(Object caller) {
+		if(caller instanceof Duel) {
+			if(Math.random() > 0.5) {
+				if (isLoaded == false) {
 					isLoaded = true;
 					return Duel.LOADING;
 				}
 				else {
-					if(Math.random() < 0.5) {
+					isLoaded = false;
 					return Duel.SHOOTING;
-					}
-					else {
-						return Duel.GUARDING;
-					}
+				}
+			}else {
+				return Duel.GUARDING;
 			}
+		}else {
 			return Duel.YEAH_RIGHT;
 		}
-		public void hit(Object caller) {
-			if(caller instanceof Duel) {
-				hp = hp-10;
-			}
+	}
+
+	public void hit(Object caller) {
+		if(caller instanceof Duel) {
+			hp = hp-10;
 		}
+	}
 }
